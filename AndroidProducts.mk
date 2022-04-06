adb6d93dece85f1de6e26e395e504946fb6111c9
IAmDeadlylxrd <contact@deadlylxrd.me>
2022-04-06 15:16:46 +0500

    mi439: Decommonize all recovery

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -21,11 +21,3 @@ COMMON_LUNCH_CHOICES := \
 	omni_mi439-user \
 	omni_mi439-userdebug \
 	omni_mi439-eng
-
-PRODUCT_MAKEFILES += \
-	$(LOCAL_DIR)/twrp_mi439.mk
-
-COMMON_LUNCH_CHOICES += \
-	twrp_mi439-user \
-	twrp_mi439-userdebug \
-	twrp_mi439-eng
--- BoardConfigPBRP.mk
+++
@@ -1,4 +0,0 @@
-# PitchBlack Recovery Project
-PB_DISABLE_DEFAULT_DM_VERITY := true
-PB_DISABLE_DEFAULT_TREBLE_COMP := true
-MAINTAINER := mi-msm8937
